5bb837ea496feefd445610bc85362fbeaed49690
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-08-19 18:02:36 -0400

    full MVP EOD Wed fontend

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const row = document.querySelector(".row")
     //be careful with log in div - maybe retitle later?
     const logInDiv = document.getElementById("login")
-    
-    
-    
+    const timeouts = []
 
     function chooseYourMonster() {
         fetch("http://localhost:3000/monsters")
@@ -37,10 +35,12 @@ document.addEventListener("DOMContentLoaded", () => {
         `
         
         newCard.dataset.id = monsterObj.id
+
         row.append(newCard)
     }
     //will erase sign in and account and create "log out button"
-    function createLogOut() {
+    function createLogOutAndUpdate() {
+        //creates log out button:
         const createAccountFormForDelete = document.getElementById("create-account")
         const signInFormForDelete = document.getElementById("sign-in")
         signInFormForDelete.remove()
@@ -50,20 +50,26 @@ document.addEventListener("DOMContentLoaded", () => {
         logOutButton.id = "logout"
         logOutButton.textContent = "Log Out"
         logInDiv.append(logOutButton)
+
+        //creates change name button:
+        const updateUserButton = document.createElement("button")
+        updateUserButton.class = "btn btn-primary"
+        updateUserButton.id = "updateName"
+        updateUserButton.textContent = "Update Username"
+        logInDiv.append(updateUserButton)
     }
-
-    
-
-    
 
     function submitHandler() {
         document.addEventListener("submit", function(e){
             const createAccountForm = document.getElementById("create-account")
             const signInForm = document.getElementById("sign-in")
             const greetingHeader = document.getElementById("greeting")
+            timeouts.forEach(clearTimeout)
+            timeouts.length = 0
+
             if(e.target === signInForm){
                 e.preventDefault()
-                
+    
                 const options = {
                     method: "POST",
                     headers: {
@@ -71,16 +77,17 @@ document.addEventListener("DOMContentLoaded", () => {
                         "Accept": "application/json"
                     },
                     body: JSON.stringify({
-                        name: signInForm.name.value
+                        email: signInForm.email.value
+            
                     })
                 }
 
                 fetch("http://localhost:3000/login", options)
                     .then(response => response.json())
                     .then(userObj => {
-                        console.log(userObj)
                         if(userObj.status){
-                        alert("Account doesn't exist. Please enter valid name or create an account")
+                        alert("Account doesn't exist. Please enter valid email or create an account.")
+                        e.target.reset()
                     }
                     else{
                         greetingHeader.dataset.id = userObj.id
@@ -92,7 +99,7 @@ document.addEventListener("DOMContentLoaded", () => {
                             chooseYourMonster()
                         }
                         
-                        createLogOut()
+                        createLogOutAndUpdate()
                     }
                     })
             } else if (e.target === createAccountForm) {
@@ -105,18 +112,24 @@ document.addEventListener("DOMContentLoaded", () => {
                         "Accept": "application/json"
                     },
                     body: JSON.stringify({
-                        name: createAccountForm.name.value
+                        name: createAccountForm.name.value,
+                        email: createAccountForm.email.value
                     })
                 }
 
                 fetch("http://localhost:3000/users", options)
                     .then(response => response.json())
                     .then(userObj => {
+                        if(userObj.status){
+                            alert("Email already taken. Please enter new email.")
+                            e.target.reset()
+                        } else {
                         greetingHeader.dataset.id = userObj.id
                         greetingHeader.textContent = `Welcome ${userObj.name}!!!!!!`
-                        
+
                         chooseYourMonster()
-                        createLogOut()
+                        createLogOutAndUpdate()
+                        }
                     })
             } else if (e.target.id === "createmonster") {
                 e.preventDefault()
@@ -150,6 +163,18 @@ document.addEventListener("DOMContentLoaded", () => {
                     .then(response => response.json())
                     .then(userMonsterObj => {renderChosenMonster(userMonsterObj,userMonsterObj.monster)})
                     
+            } else if(e.target.id === "updatenameform") {
+                
+                e.preventDefault()
+                
+                const userId = parseInt(document.getElementById("greeting").dataset.id)
+                const updatedName = {
+                    name: e.target.updatedname.value
+                }
+                patchUser(userId, updatedName)
+                
+                //const logOutButton = document.createElement("button")
+                e.target.remove()                    
             }
 
 
@@ -177,11 +202,35 @@ document.addEventListener("DOMContentLoaded", () => {
            
 
     }
+
+
+
+    function patchUser(userId, attrObject) {
+        
+        const options = {
+            method: "PATCH",
+            headers: {
+                "Content-Type":"application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(
+                attrObject
+            )
+        }
+
+        fetch(`http://localhost:3000/users/${userId}`, options)
+            .then(response => response.json())
+            .then(updatedUser => {
+                document.getElementById("greeting").textContent = `Welcome ${updatedUser.name}!!!!!!`
+
+            })
+           
+
+    }
+
     function renderChosenMonster(pet,monsterTemplate) {
-        // get attributes off user_monster and fill here
         let {happiness, hunger_level, power} = pet
-        console.log("this ran")
-        console.log(pet,monsterTemplate)
+        
         row.innerHTML = `
         <div data-id=${pet.id} class="card" style="width: 45rem;">
             <img src=${monsterTemplate.image_url} class="card-img-top" alt="...">
@@ -190,51 +239,76 @@ document.addEventListener("DOMContentLoaded", () => {
             <p class="card-text">${pet.name} loves long walks on the beach</p>
             <button type="button" class="btn btn-info">Feed</button><br><br>
                 <div class="progress">
-                    <div class="progress-bar progress-bar-striped bg-info" data-name="hunger" role="progressbar" style="width: ${hunger_level}%" aria-valuenow="${hunger_level}" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar progress-bar-striped bg-info" data-name="hunger" role="progressbar" style="width: ${hunger_level}%" aria-valuenow="${hunger_level}" aria-valuemin="0" aria-valuemax="100">HUNGER</div>
                 </div><br><br>
     
-                <button type="button" class="btn btn-warning">Pet 😸</button>
-                <button type="button" class="btn btn-warning">Punish 😿</button><br><br>
+                <button type="button" class="btn btn-warning">Punish 😿</button>
+                <button type="button" class="btn btn-warning">Pet 😸</button><br><br>
+                
                 <div class="progress">
-                    <div class="progress-bar progress-bar-striped bg-warning" data-name="happiness" role="progressbar" style="width: ${happiness}%" aria-valuenow="${happiness}" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar progress-bar-striped bg-warning" data-name="happiness" role="progressbar" style="width: ${happiness}%; color:black" aria-valuenow="${happiness}" aria-valuemin="0" aria-valuemax="100">HAPPINESS</div>
                 </div><br><br>    
     
     
-                <button type="button" class="btn btn-danger">Train</button><br><br>
+                <button type="button" class="btn btn-success">Train</button><br><br>
                 <div class="progress">
-                    <div class="progress-bar progress-bar-striped bg-danger" data-name="power" role="progressbar" style="width: ${power}%" aria-valuenow="${power}" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar progress-bar-striped bg-success" data-name="power" role="progressbar" style="width: ${power}%" aria-valuenow="${power}" aria-valuemin="0" aria-valuemax="100">POWER</div>
                 </div><br><br>
+
+                <button type="button" class="btn btn-danger">Release Monster</button><br><br>
             </div>
         </div>
         `
+    
+
+        timeouts.push(setTimeout(function(){   
+        let currentTimerStat = parseInt(hunger_level)
+            currentTimerStat-=10
+                
+        const newTimerStatObj = {
+            hunger_level: currentTimerStat
+            }
+
+        patchMonster(pet.id, newTimerStatObj)
+            }, 15000)   
+        )
+
+
     }
 
    function clickHandler() {
        document.addEventListener("click", function(e){
+            
             if (e.target.textContent === "Log Out") {
+                timeouts.forEach(clearTimeout)
+                timeouts.length = 0
                 row.innerHTML = ``
-                //logOutButton.remove()
                 logInDiv.innerHTML = `
                 <h3 id="greeting"></h3>
                 <form class="form-inline" id="sign-in">
                     <div class="form-group">
-                        <label class="sr-only" for="name">Username</label>
-                        <input type="text" class="form-control" id="name" placeholder="Username">
+                        <label class="sr-only" for="name">Email</label>
+                        <input type="text" class="form-control" id="email" placeholder="Email">
                     </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
-                    </form><br>
-                    <form class="form-inline" id="create-account">
-                <div class="form-group">
-                    <label class="sr-only" for="name">Enter Username</label>
-                    <input type="text" class="form-control" id="name" placeholder="Username">
-                </div>
-                <button type="submit" class="btn btn-primary">Create Account</button>
-                    </form>                
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </form><br>
+                <form class="form-inline" id="create-account">
+                    <div class="form-group">
+                        <label class="sr-only" for="email">Enter Email</label>
+                        <input type="text" class="form-control" id="email" placeholder="Email">
+                        <label class="sr-only" for="name">Enter Name</label>
+                        <input type="text" class="form-control" id="name" placeholder="Name">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Create Account</button>
+                </form>            
                 `
             } else if(e.target.textContent === "Feed") {
                 const userMonsterId = e.target.closest(".card").dataset.id
                 const progressBar = e.target.parentElement.querySelector("[data-name='hunger']")
-                
+
+                timeouts.forEach(clearTimeout)
+                timeouts.length = 0
+
                 let currentStat = parseInt(progressBar.ariaValueNow)
                 currentStat+=5
                 
@@ -248,6 +322,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 const userMonsterId = e.target.closest(".card").dataset.id
                 const progressBar = e.target.parentElement.querySelector("[data-name='happiness']")
                 
+                timeouts.forEach(clearTimeout)
+                timeouts.length = 0
+
                 let currentStat = parseInt(progressBar.ariaValueNow)
                 currentStat+=5
                 
@@ -261,6 +338,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 const userMonsterId = e.target.closest(".card").dataset.id
                 const progressBar = e.target.parentElement.querySelector("[data-name='happiness']")
                 
+                timeouts.forEach(clearTimeout)
+                timeouts.length = 0
+
                 let currentStat = parseInt(progressBar.ariaValueNow)
                 currentStat-=5
                 
@@ -274,6 +354,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 const userMonsterId = e.target.closest(".card").dataset.id
                 const progressBar = e.target.parentElement.querySelector("[data-name='power']")
                 
+                timeouts.forEach(clearTimeout)
+                timeouts.length = 0
+
                 let currentStat = parseInt(progressBar.ariaValueNow)
                 currentStat+=5
                 
@@ -282,10 +365,45 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
 
                 patchMonster(userMonsterId, newStatObj)
+
+            } else if (e.target.textContent === "Update Username") {
+                const nameForm = document.createElement("form")
+                nameForm.id = "updatenameform"
+                nameForm.innerHTML = `
+                    <div class="form-group">
+                        <label class="sr-only" for="name">Update Name</label>
+                        <input type="text" class="form-control" id="updatedname" placeholder="Enter New Username">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Update Name</button>
+                `
+                logInDiv.append(nameForm)
+               
+            } else if (e.target.textContent === "Release Monster") {
+                timeouts.forEach(clearTimeout)
+                timeouts.length = 0
+                
+                const userMonsterId = e.target.closest(".card").dataset.id
+                
+                const deleteObj = {
+                    method: 'DELETE',
+                  }
+
+                fetch(`http://localhost:3000/user_monsters/${userMonsterId}`, deleteObj)
+                .then(response => response.json())
+                .then(emptyObj => {
+                    if(emptyObj.status) {
+                        alert("Delete did not work, please refresh page.")
+                    } else {
+                        row.innerHTML = ``
+                        chooseYourMonster()
+                    }
+
+                })
+                
             }
            
        })
-   }
+    }
 
    
     clickHandler()
